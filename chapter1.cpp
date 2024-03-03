9d06dd080d9c090d8c6bcadb553086f2430a4fa7
Dyutimoy Biswas <mail.dybiswas@gmail.com>
2024-03-03 23:26:44 +0530

    chapter1

---
+++ chapter1.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+
+int main(void) {
+    // Excercise 1.2
+    // return -1; // prints 255
+
+    // Excercise 1.3
+    std::cout << "Hello, World!" << std::endl;
+
+    // Excercise 1.4
+    int a = 0, b = 0;
+    std::cin >> a >> b;
+    std::cout << "The product of " << a
+    << " and " <<b
+    << " is " << a*b << std::endl;
+
+    // Excercise 1.9
+    int sum = 0, m = 50;
+    while (m <= 100) {
+        sum += m;
+        m++;
+    }
+    std::cout << "Sum of numbers from 50 to 100 is " << sum << std::endl;
+
+    // Excercise 1.10
+    int n = 10;
+    while (n >= 0) {
+        std::cout << n << " ";
+        n--;
+    }
+    std::cout << std::endl;
+
+    // Excercise 1.12
+    sum = 0;
+    for (int i = -100; i <= 100; ++i) sum += i;
+    std::cout << sum << std::endl;  // 0
+
+    // Excercise 1.16
+    // int x; sum = 0;
+    // while (std::cin >> x) {
+    //     sum += x;
+    // }
+    // std::cout << sum << std::endl;
+
+    // Assignment can appear as a condition
+    if (int x = 5) std::cout << "Always executes." << std::endl;
+
+    // Never executes.
+    if (int x = 't') {} else {}
+
+    // Print numbers as they are entered and calculate frequencies in real time.
+    int value = 0, lastValue = 0;
+    if (std::cin >> lastValue) {    // To ensure at least one value has been entered.
+        int count = 1;
+        while (std::cin >> value) {
+            if (value == lastValue) {   // Value is repeated.
+                count++;
+            } else {    // Print value as well as its frequency.
+                std::cout << "Value - " << lastValue << " occurs " << count << " times." << std::endl;
+                lastValue = value;
+                count = 1;
+            }
+        }
+        // To print the last occurring value.
+        std::cout << "Value - " << lastValue << " occurs " << count << " times." << std::endl;
+    }
+
+}
