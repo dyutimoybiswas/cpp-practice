744bc7190d54d5b7caa3b385e6d4bfc668a5fb4f
Dyutimoy Biswas <mail.dybiswas@gmail.com>
2024-08-10 12:41:14 +0530

    added ifdefs for exercises in chapter 1

--- chapter1/chapter1.cpp
+++ chapter1/chapter1.cpp
@@ -2,50 +2,61 @@
 
 int main(void) {
     // Excercise 1.2
-    #ifdef EX1_2
+    #ifdef EXERCISE2
     return -1; // prints 255
     #endif
 
     // Excercise 1.3
+    #ifdef EXERCISE3
     std::cout << "Hello, World!" << std::endl;
+    #endif
 
     // Excercise 1.4
+    #ifdef EXERCISE4
     int a = 0, b = 0;
     std::cin >> a >> b;
     std::cout << "The product of " << a
     << " and " <<b
     << " is " << a*b << std::endl;
+    #endif
 
     // Excercise 1.9
+    #ifdef EXERCISE9
     int sum = 0, m = 50;
     while (m <= 100) {
         sum += m;
         m++;
     }
     std::cout << "Sum of numbers from 50 to 100 is " << sum << std::endl;
+    #endif
 
     // Excercise 1.10
+    #ifdef EXERCISE10
     int n = 10;
     while (n >= 0) {
         std::cout << n << " ";
         n--;
     }
     std::cout << std::endl;
+    #endif
 
     // Excercise 1.12
-    sum = 0;
+    #ifdef EXERCISE12
+    int sum = 0;
     for (int i = -100; i <= 100; ++i) sum += i;
     std::cout << sum << std::endl;  // 0
+    #endif
 
     // Excercise 1.16
-    #ifdef EX1_16
-    int x; sum = 0;
+    #ifdef EXERCISE16
+    int x, sum = 0;
     while (std::cin >> x) {
         sum += x;
     }
     std::cout << sum << std::endl;
     #endif
 
+    #ifdef CUSTOM
     // Assignment can appear as a condition
     if (int x = 5) std::cout << "Always executes." << std::endl;
 
@@ -65,5 +76,5 @@ int main(void) {
         // To print the last occurring value.
         std::cout << "Value - " << lastValue << " occurs " << count << " times." << std::endl;
     }
-
+    #endif
 }
