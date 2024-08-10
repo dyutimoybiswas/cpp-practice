16be7d44a1a05bec27162f05792252013609a8e4
Dyutimoy Biswas <mail.dybiswas@gmail.com>
2024-08-10 12:40:52 +0530

    updated run_file

--- run_file.sh
+++ run_file.sh
@@ -1,16 +1,31 @@
-# Provide folder name as argument eg - bash run_file part2 chapter 9 to run exercises in chapter 9.
-# Additionally define exercises to run as -D EXCHNO_EXNO where CHNO - chapter no, EXNO - exercise no
-# eg -D EX_3_20 to run Ex 3.20
+# Run as - bash run_file part_no chapter_no exercise_no eg - bash run_file 2 9 4 to run exercise 4 in chapter 9 under part 2.
+# Define CUSTOM to run examples.
 # Part1 contains chapters 2 through 7.
 # Part2 contains chapters 8 through 12.
 # Part3 contains chapters 13 through 16.
 # Part4 contains chapters 17 through 19.
 
-if [ $# -lt 2 ]; then
-    echo "Too few arguments. Please specify part no and chapter no."
+if [ $# -lt 3 ]; then
+    # Special case - chapter 1
+    if [ $1 -eq 1 ]; then
+        if [ $# -lt 2 ]; then
+            echo "Too few arguments. Please specify exercise no."
+        else
+            EXERCISE=$2
+            g++ -std=c++20 -o output chapter1/*.cpp -D "EXERCISE${EXERCISE} "
+            ./output
+        fi
+    else
+        echo "Too few arguments. Please specify part no, chapter no and exercise no."
+    fi
 else
     PART=$1
     CHAPTER=$2
-    g++ -std=c++20 -o output $PART/$CHAPTER/*.cpp
+    if [ $3 = CUSTOM ]; then
+        g++ -std=c++20 -o output "part${PART}"/"chapter${CHAPTER}"/*.cpp -D CUSTOM
+    else
+        EXERCISE=$3
+        g++ -std=c++20 -o output "part${PART}"/"chapter${CHAPTER}"/*.cpp -D "EXERCISE${EXERCISE}"
+    fi
     ./output
 fi
