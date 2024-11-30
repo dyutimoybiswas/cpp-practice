dd3e031706291cb3d769b42130b5a8bfa04f244d
Dyutimoy Biswas <mail.dybiswas@gmail.com>
2024-11-30 11:14:03 +0530

    updated run_file

--- multithreading/run_file.sh
+++ multithreading/run_file.sh
@@ -1,4 +1,8 @@
 
 # Provide <file_name.cpp> as argument while running this script.
 g++ -std=c++20 -O1 -pthread -o output $1
-./output
+
+# If command line values passed, run manually.
+if [[ $# -gt 1 && $2 != "manual" ]] || [[ $# -eq 1 ]]; then
+    ./output
+fi
